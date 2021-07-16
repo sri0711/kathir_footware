c611a75dd93ff61a9f0152dade0d72cec85877d6
sri0711 <m.sathish0711@gmail.com>
2021-07-16 19:26:09 +0530

    UPDATE:mail sender finished

--- library/mainler.js
+++ library/mainler.js
@@ -4,15 +4,21 @@ const mailer = require('nodemailer')
 
 console.log(process.env)
 
-const mailSender = async (props)=>{
+const mailSender = async ()=>{
+
+    // This is main section for the mailing function start
+
+    // mail sending provider configuration
 
     const mailTranspoter = mailer.createTransport({
-        host:'gmail',
+        service:'gmail',
         auth:{
             user:'padhu.developer@gmail.com',
             pass:process.env.PASS
         }
     })
+
+    //  Mail sending properties
 
     const bodyMessage = await transporter.sendMail({
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
@@ -22,5 +28,13 @@ const mailSender = async (props)=>{
         Please visit again`, // plain text body
         html: `<h1>This is the Sample of the project</h1>`, // html body
       });
+
+    // main send and return back messages for the packages
+
+    await mailTranspoter.sendMail(bodyMessage,(err,info)=>{
+        if(err) console.log(err)
+        else console.log(info)
+
+    })
 
 }
